d61bc82f5fd929531b992f61a985ce40dc02f59e
devanshu0x <devanshudotdev@gmail.com>
2025-06-26 20:07:36 +0530

    add sidebar and appbar

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@repo/db/client";
+import express from "express"
+
+const app=express();
+
+app.post("/hdfcWebhook", async (req,res)=>{
+    const paymentInformation={
+        token:req.body.token,
+        userId:req.body.user_identifier,
+        amount:req.body.amount
+    }
+
+    try{
+        await prisma.$transaction([
+        prisma.balance.update({
+        where:{
+            userId: paymentInformation.userId
+        },
+        data:{
+            amount:{
+                increment:paymentInformation.amount
+            }
+        }
+    }),
+
+    prisma.onRampTransition.update({
+        where:{
+            token:paymentInformation.token
+        },
+        data:{
+            status:"Success"
+        }
+    })
+        ])
+
+    res.json({
+        message:"Captured"
+    })
+        
+    }catch(e){
+        res.status(411).json({
+            message:"Error while processing webhook"
+        })
+    }
+
+
+})
--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "../provider";
+import { AppbarClient } from "../AppbarClient";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Providers>
+          <AppbarClient/>
         {children}
+        </Providers>
       </body>
     </html>
   );
